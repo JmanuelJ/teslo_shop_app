cc9879fda08a2f28aaeabd4583c104530d2f4d2a
Juan Manuel Juarez <penta21zona15@gmail.com>
2025-07-19 23:49:00 -0600

    auth screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,10 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:teslo_shop/features/auth/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/login', routes: [
   // Auth Routes
-  GoRoute(path: '/login', builder: (context, state) => const Scaffold()),
-  GoRoute(path: '/register', builder: (context, state) => const Scaffold()),
+  GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+  GoRoute(path: '/register', builder: (context, state) => const RegisterScreen()),
 
   //Product Routes
   GoRoute(path: '/', builder: (context, state) => const Scaffold())
